28cde0e10a2d353fa8199ffbb023fdb951eeaeb9
fafmeirelles <88750130+fafmeirelles@users.noreply.github.com>
2026-09-08 11:33:53 -0300

    Add files via upload

---
+++ AlgoritmoOito.java
@@ -0,0 +1,10 @@
+public class AlgoritmoOito {
+void main() {
+        int num1 = Integer.parseInt(IO.readln("Informe o Primeiro Número: "));
+        int num2 = Integer.parseInt(IO.readln("Informe outro Número: "));
+
+        System.out.println("Produto: " + (num1 * num2));
+
+
+    }
+}
---
+++ AlgoritmoSete.java
@@ -0,0 +1,8 @@
+public class AlgoritmoSete {
+void main() {
+        int num1 = Integer.parseInt(IO.readln("Informe um Número: "));
+        int num2 = Integer.parseInt(IO.readln("Informe outro Número: "));
+        
+        System.out.println("Soma: " + (num1 + num2));
+    }
+}
