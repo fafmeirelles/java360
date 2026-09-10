693508ea08a2055d0a7164080527f580fc317efb
fafmeirelles <88750130+fafmeirelles@users.noreply.github.com>
2026-09-10 09:24:50 -0300

    Add files via upload

---
+++ AlgoritmoVinteEDois.java
@@ -0,0 +1,33 @@
+public class AlgoritmoVineEDois {
+    /*O Banco SENATECH é um banco que auxilia ex-alunos que implementa e desenvolve Startups. A SENATECH quer que você desenvolva de forma simples um sistema 
+    //que recebe a usuário e a senha (para teste considere usuario:senai senha:123). A lógica funciona assim: se ele errar três vezes você deve bloquear o 
+    // //cartão e pedir para ele procurar o gerente caso contrário abra uma janela perguntando : quando deseja simular de empréstimo.  O empréstimo máximo é 
+    // //20.000. Pergunte a quantidade de parcelas e mostre o valor das parcelas com juros de 1% ao mês. As parcelas podem ser divididas em até 10 meses. 
+    // //Mostre o valor total do financiamento, o valor financiado e a mensagem "Parabéns, seja bem vindo a família SENATECH. */
+    
+//import java.util.ArrayList<ControleDeEntrada> List = new java.util.Arraylist<ControleDeEntrada>();
+
+    public static vois main(String[] args){
+        String nome;
+        Int senha = 123;
+        IO.println("Nome do Usuário");
+        IO.println("Senha");
+        for(int i = 0; i<= 3){
+            if(nome != "senai" && senha != 123){
+                IO.println("Nome e senhas arrados, tente outra vez")
+                i++;{
+                    else if(i = 3){
+                        IO.println("Terceira entativa, CARTÃO bloqueado, procure o Gerente"){
+                            else{
+                                IO.println("Seseja simular um empréstimo");
+                                IO.println("O limite máximo é de R$20.000,00");
+                                IO.println("Em quantas parcelas voceê deseja, com juro de 1% ao mês");+
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AlgoritmoVinteEUm.java
@@ -0,0 +1,9 @@
+public class AlgoritmoVinteEUm {
+    public static void main(String[] args) {
+        int valor[] = new int i [45, 67, 89, 34, 23, 56 , 78, 98, 45, 34];
+        for(int i = 0; i < valor; i++){
+           int total = valor + i;
+                System.out.println(total);
+            }
+        }
+}
